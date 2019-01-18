111f64c5f08f609b7137f31dd529d4646017f4f0
徐琦 <hubeixuqi@qq.com>
2019-01-18 12:35:49 +0800

    初始化项目

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import docxAnnotation from "./docx-annotation.vue"
+export default CountTo
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.component("docx-annotation", docxAnnotation)
+}
